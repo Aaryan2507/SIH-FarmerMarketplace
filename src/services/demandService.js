10488996c406bac2b8dcfa95270d49fee55afe14
Aaryan2507 <sainiaaryan2007@gmail.com>
2026-08-28 16:07:53 +0530

    Delete src directory

--- src/services/demandService.js
+++
@@ -1,7 +0,0 @@
-import { simulateLatency } from "./api"
-import { mockDemand } from "@/data/mockDemand"
-
-export async function listDemandInsights() {
-  await simulateLatency()
-  return mockDemand
-}
